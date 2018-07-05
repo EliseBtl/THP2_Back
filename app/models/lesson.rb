54f0135b6c9c72973c542aaa3d24ec252abd19d4
Elise <elisebal4@gmail.com>
2018-07-05 17:43:52 +0200

    model and validations

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :uuid             not null, primary key
+#  title       :string(50)       not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Lesson < ApplicationRecord
+  validates :title, :description, presence: true
 end
