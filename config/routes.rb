cfa3da120da0a8ac849294eebdbd7be6055e6b50
Elise <elisebal4@gmail.com>
2018-07-12 14:11:35 +0200

    Routing: resource must have everything but new and edit (pas sûre de moi)

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ require 'bootsnap/setup'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lessons, only: %i[index show destroy]
 end
---
+++ spec/controllers/lesson_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe LessonController, type: :controller do
+end
