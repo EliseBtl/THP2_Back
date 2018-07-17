15a4310953bea95cade9895972709cf08f42ccb6
Elise <elisebal4@gmail.com>
2018-07-17 10:37:08 +0200

    controller, jeson_helper and tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'support/factory_bot'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -56,6 +57,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+  config.include JsonHelper
 end
 
 Shoulda::Matchers.configure do |config|
